3c92da9ac7fa4c89cd160e76f1f4f8a39fe1a816
oliviatatum <olivia.tatum1@hotmail.com>
2019-11-25 15:51:34 +0000

    added Catulator

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var amountOfWeightLost = document.getElementById("weightLost");
+var kittenWeight = 0.5;
+var kittenWeightInGrams = 226.796;
+
+function toKittens() {
+    a/b
+}
+
+function toKittens() {
+    var kittens = parseInt( document.getElementById("weightLost").value ) / kittenWeight;
+    console.log(kittens);
+    var text = `<h3 class="display-2">That's ${kittens} kittens!</h3>`;  
+    document.getElementById("kittenResult").innerHTML = text;    
+}
+
+
+
+
+
+
+
