f31eec8c1f4c90f813809561baa220bf8e587d4c
oliviatatum <olivia.tatum1@hotmail.com>
2019-11-28 14:50:25 +0000

    added unit options to Cat-ulator

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 var amountOfWeightLost = document.getElementById("weightLost");
 var kittenWeight = 0.5;
-var kittenWeightInGrams = 226.796;
+var kittenWeightInKilograms = 0.226796;
+
+
+
 
 function toKittens() {
-    a/b
-}
 
-function toKittens() {
-    var kittens = parseInt( document.getElementById("weightLost").value ) / kittenWeight;
+    var unit_selection = document.getElementById("inputGroupSelect04").value;
+
+    if (unit_selection == "lbs") {
+        kittenW = kittenWeight;
+    } else if (unit_selection == "kg") {
+        kittenW = kittenWeightInKilograms;
+    }
+
+    var kittens = parseInt( document.getElementById("weightLost").value ) / kittenW;
     console.log(kittens);
     var text = `<h3 class="display-2">That's ${kittens} kittens!</h3>`;  
     document.getElementById("kittenResult").innerHTML = text;    
 }
-
-
-
-
-
-
-
