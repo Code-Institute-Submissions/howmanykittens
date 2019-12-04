d8ac83e5f625287f4b47868d62233f564beb0e73
oliviatatum <olivia.tatum1@hotmail.com>
2019-12-04 10:32:43 +0000

    added loop to produce kittens

--- js/main.js
+++ js/main.js
@@ -1,22 +1,75 @@
 
-
-
-
-
-function toKittens() {
-
-    var unit_selection = document.getElementById("inputGroupSelect04").value;
-    var kittenWeightInLb = 0.5;
-    var kittenWeightInKilograms = 0.226796;
-
-    if (unit_selection == "lbs") {
-        kittenW = kittenWeightInLb;
-    } else if (unit_selection == "kg") {
-        kittenW = kittenWeightInKilograms;
-    }
-
-    var kittens = parseInt(parseInt(document.getElementById("weightLost").value ) / kittenW);
-    console.log(kittens);
-    var text = `<h3 class="display-2">That's ${kittens} kittens!</h3>`;  
-    document.getElementById("kittenResult").innerHTML = text;    
+/* Retrieve the unit selection from the user (lb/kg) */
+function getUnitFromUser() {
+	return document.getElementById("inputGroupSelect04").value;
 }
+
+/* Retrieve weight entered by user */
+function getWeightFromUser() {
+  return parseInt(document.getElementById("weightLost").value);
+}
+
+/* Selects kitten weight based on unit selected */
+function getKittenWeight(unit) {
+  var kittenW;
+  if (unit == "lbs") {
+    kittenW = 0.5; // kittenWeightInLb
+  } else if (unit == "kg") {
+    kittenW =  0.226796; // kittenWeightInKilograms
+  }
+  return kittenW;
+}
+
+/* Catulator - converts user weight into kitten amount */
+function calculateKittens(unit) {
+  return parseInt(getWeightFromUser() / getKittenWeight(unit));
+}
+
+/* Display the result */
+function outputResult(kittens) {
+  var text = `<h3 class="display-2">That's ${kittens} kittens!</h3>`;
+  document.getElementById("kittenResult").innerHTML = text;
+}
+
+/* Kitten interval delay */
+function kittenTimings(func) {
+
+    setTimeout(func, 5000);
+
+}
+
+/*For loop to produce kittens*/
+function outputKittens(amount){
+    for(var i = 0; i <amount; i++){
+    (function(i){
+        setTimeout(function(){
+            alert("KITTENS!");
+        }, 3000 * (i + 1));
+    })(i);
+}
+}
+/* Invokes the functions to put it all together */
+function toKittens() {
+  var unit_selection = getUnitFromUser();
+
+  var kittens = calculateKittens(unit_selection);
+
+  console.log(kittens);
+
+  outputResult(kittens);
+  
+  outputKittens(kittens);
+  
+}
+
+/* Listens for the event of the Go button being clicked and calls the toKittens function */
+document.getElementById("go").addEventListener("click", toKittens);
+
+
+
+
+
+
+
+
+
